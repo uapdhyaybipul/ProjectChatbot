8691f82560beb7f9cf4833ec6ee4f01db299264f
uapdhyaybipul <upadhyaybipul142@gmail.com>
2026-02-22 05:59:26 +0530

    login router

--- src/app/api/routes_auth.py
+++ src/app/api/routes_auth.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+from fastapi import APIRouter
+from pydantic import BaseModel
+from src.core.security import create_access_token
+from src.core.dependencies import get_current_user
 
-app = FastAPI()
+router = APIRouter()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class AuthRequest(BaseModel):
+    username: str
+    password: str
 
+@router.post("/login")
+async def login(request: AuthRequest):
+    if request.username != "admin" or request.password != "admin":
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    else:
+        access_token = create_access_token(data={"sub": request.username})
+        return {"access_token": access_token, "token_type": "bearer"}
